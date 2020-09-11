f610c7cb42ace481c553160ce76972e1e586030a
tomsnews <49147550+tomsnews@users.noreply.github.com>
2020-09-11 12:46:02 +0200

    Initial Version

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,23 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        ' Get Sender
+        Dim srcButton As Button
+        srcButton = CType(e.Source, Button)
+
+        ' Find SoundName
+        Dim SoundName As String
+        SoundName = srcButton.Tag
+
+        ' Convert Name to Object
+        Dim SoundObj = My.Resources.ResourceManager.GetObject(SoundName)
+        My.Computer.Audio.Play(SoundObj, AudioPlayMode.Background)
+
+    End Sub
+    Private Sub ResetDesign(sender As Object, e As MouseButtonEventArgs)
+        Me.Width = 480
+        Me.Height = 950
+    End Sub
+
+End Class
+
